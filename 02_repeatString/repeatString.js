a186605d3c20b6284f3e576fe57ba7ba27e8ca6b
JCEE-Raven <170010059+JCEE-Raven@users.noreply.github.com>
2024-06-14 16:18:26 +0100

    Edit repeatString Function
    Added code to the repeat string function to ad conditions and a loop
    so the string can be repeated properly and the tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, repeatTimes) {
+    
+    let stringToReturn = stringToRepeat;
 
+    if(repeatTimes == 0) {
+        return "";
+        
+    }else if(repeatTimes < 0) {
+        return "ERROR";
+
+    }else{
+        for(let i = 1; i < repeatTimes; i++) {
+            stringToReturn = stringToReturn + stringToRepeat;
+        }
+    
+        return stringToReturn;
+    }
+    
+    
 };
 
 // Do not edit below this line
