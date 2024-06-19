fb3193ae6771ca93c567cb7c60733e857342354e
JCEE-Raven <170010059+JCEE-Raven@users.noreply.github.com>
2024-06-19 12:44:16 +0100

    Edit remove from function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...toRemove) {
+    let outputArray = [];
+
+    inputArray.forEach((item) => {
+        if(!toRemove.includes(item)) {
+            outputArray.push(item);
+        }
+    })
+
+    return outputArray;
 };
 
 // Do not edit below this line
